9963d632a19c661b8d28957938538f498cb71647
Tanay Upadhyaya <tanayupadhyaya@gmail.com>
2023-03-05 17:31:52 +0530

    Minor cleanup and added TODO comments

--- app/faster_whisper/utils.py
+++ app/faster_whisper/utils.py
@@ -1,20 +1,6 @@
 import json
 import os
-import sys
-import zlib
-from typing import Callable, TextIO
-
-system_encoding = sys.getdefaultencoding()
-
-if system_encoding != "utf-8":
-    def make_safe(string):
-        # replaces any character not representable using the system default encoding with an '?',
-        # avoiding UnicodeEncodeError (https://github.com/openai/whisper/discussions/729).
-        return string.encode(system_encoding, errors="replace").decode(system_encoding)
-else:
-    def make_safe(string):
-        # utf-8 can encode any Unicode code point, so no need to do the round-trip encoding
-        return string
+from typing import TextIO
 
 
 def format_timestamp(seconds: float, always_include_hours: bool = False, decimal_marker: str = '.'):
--- model_conversion.sh
+++ model_conversion.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# TODO: Add step to build setup based on ENV variable
 ct2-transformers-converter --model openai/whisper-large-v2 --output_dir /root/.cache/faster_whisper --quantization float16
