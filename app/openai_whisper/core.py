f704e3ca57dce51e373443930475ac4908f25234
vyacheslav-voloshyn <vyacheslav.voloshyn@economia.cz>
2023-07-14 07:46:08 +0200

    Try to refactoring/clean up the code

--- app/openai_whisper/core.py
+++ app/openai_whisper/core.py
@@ -7,12 +7,14 @@ import torch
 import whisper
 from whisper.utils import ResultWriter, WriteTXT, WriteSRT, WriteVTT, WriteTSV, WriteJSON
 
-model_name= os.getenv("ASR_MODEL", "base")
-if torch.cuda.is_available():
-    model = whisper.load_model(model_name).cuda()
-else:
-    model = whisper.load_model(model_name)
-model_lock = Lock()
+def initialize_model():
+    model_name = os.getenv("ASR_MODEL", "base")
+    if torch.cuda.is_available():
+        model = whisper.load_model(model_name).cuda()
+    else:
+        model = whisper.load_model(model_name)
+    model_lock = Lock()
+    return model, model_lock
 
 def transcribe(
     audio,
@@ -22,13 +24,15 @@ def transcribe(
     word_timestamps: Union[bool, None],
     output
 ):
+    model, model_lock = initialize_model()
+
     options_dict = {"task" : task}
     if language:
         options_dict["language"] = language
     if initial_prompt:
         options_dict["initial_prompt"] = initial_prompt
     with model_lock:
-        result = model.transcribe(audio, **options_dict)
+        result = model.transcribe(audio, fp16=False , **options_dict)
 
     outputFile = StringIO()
     write_result(result, outputFile, output)
@@ -37,13 +41,11 @@ def transcribe(
     return outputFile
 
 def language_detection(audio):
-    # load audio and pad/trim it to fit 30 seconds
-    audio = whisper.pad_or_trim(audio)
+    model, model_lock = initialize_model()
 
-    # make log-Mel spectrogram and move to the same device as the model
+    audio = whisper.pad_or_trim(audio)
     mel = whisper.log_mel_spectrogram(audio).to(model.device)
 
-    # detect the spoken language
     with model_lock:
         _, probs = model.detect_language(mel)
     detected_lang_code = max(probs, key=probs.get)
@@ -53,15 +55,16 @@ def language_detection(audio):
 def write_result(
     result: dict, file: BinaryIO, output: Union[str, None]
 ):
-    if(output == "srt"):
-        WriteSRT(ResultWriter).write_result(result, file = file)
-    elif(output == "vtt"):
-        WriteVTT(ResultWriter).write_result(result, file = file)
-    elif(output == "tsv"):
-        WriteTSV(ResultWriter).write_result(result, file = file)
-    elif(output == "json"):
-        WriteJSON(ResultWriter).write_result(result, file = file)
-    elif(output == "txt"):
-        WriteTXT(ResultWriter).write_result(result, file = file)
+    writers = {
+        "srt": WriteSRT(ResultWriter).write_result,
+        "vtt": WriteVTT(ResultWriter).write_result,
+        "tsv": WriteTSV(ResultWriter).write_result,
+        "json": WriteJSON(ResultWriter).write_result,
+        "txt": WriteTXT(ResultWriter).write_result
+    }
+
+    writer_func = writers.get(output)
+    if writer_func:
+        writer_func(result, file=file)
     else:
         return 'Please select an output method!'
--- app/webservice.py
+++ app/webservice.py
@@ -17,8 +17,8 @@ if ASR_ENGINE == "faster_whisper":
 else:
     from .openai_whisper.core import transcribe, language_detection
 
-SAMPLE_RATE=16000
-LANGUAGE_CODES=sorted(list(tokenizer.LANGUAGES.keys()))
+SAMPLE_RATE = 16000
+LANGUAGE_CODES = sorted(list(tokenizer.LANGUAGES.keys()))
 
 projectMetadata = importlib.metadata.metadata('whisper-asr-webservice')
 app = FastAPI(
@@ -54,34 +54,35 @@ async def index():
 
 @app.post("/asr", tags=["Endpoints"])
 def asr(
-    task : Union[str, None] = Query(default="transcribe", enum=["transcribe", "translate"]),
+    task: Union[str, None] = Query(default="transcribe", enum=["transcribe", "translate"]),
     language: Union[str, None] = Query(default=None, enum=LANGUAGE_CODES),
     initial_prompt: Union[str, None] = Query(default=None),
     audio_file: UploadFile = File(...),
-    encode : bool = Query(default=True, description="Encode audio first through ffmpeg"),
-    output : Union[str, None] = Query(default="txt", enum=["txt", "vtt", "srt", "tsv", "json"]),
-    word_timestamps : bool = Query(
-        default=False, 
-        description="World level timestamps", 
+    encode: bool = Query(default=True, description="Encode audio first through ffmpeg"),
+    output: Union[str, None] = Query(default="txt", enum=["txt", "vtt", "srt", "tsv", "json"]),
+    word_timestamps: bool = Query(
+        default=False,
+        description="World level timestamps",
         include_in_schema=(True if ASR_ENGINE == "faster_whisper" else False)
     )
 ):
     result = transcribe(load_audio(audio_file.file, encode), task, language, initial_prompt, word_timestamps, output)
     return StreamingResponse(
-        result, 
-        media_type="text/plain", 
+        result,
+        media_type="text/plain",
         headers={
                 'Asr-Engine': ASR_ENGINE,
                 'Content-Disposition': f'attachment; filename="{audio_file.filename}.{output}"'
-            })
+            }
+    )
 
 @app.post("/detect-language", tags=["Endpoints"])
 def detect_language(
     audio_file: UploadFile = File(...),
-    encode : bool = Query(default=True, description="Encode audio first through ffmpeg")
+    encode: bool = Query(default=True, description="Encode audio first through ffmpeg")
 ):
     detected_lang_code = language_detection(load_audio(audio_file.file, encode))
-    return { "detected_language": tokenizer.LANGUAGES[detected_lang_code], "language_code" : detected_lang_code }
+    return {"detected_language": tokenizer.LANGUAGES[detected_lang_code], "language_code": detected_lang_code}
 
 def load_audio(file: BinaryIO, encode=True, sr: int = SAMPLE_RATE):
     """
@@ -101,8 +102,6 @@ def load_audio(file: BinaryIO, encode=True, sr: int = SAMPLE_RATE):
     """
     if encode:
         try:
-            # This launches a subprocess to decode audio while down-mixing and resampling as necessary.
-            # Requires the ffmpeg CLI and `ffmpeg-python` package to be installed.
             out, _ = (
                 ffmpeg.input("pipe:", threads=0)
                 .output("-", format="s16le", acodec="pcm_s16le", ac=1, ar=sr)
@@ -114,3 +113,4 @@ def load_audio(file: BinaryIO, encode=True, sr: int = SAMPLE_RATE):
         out = file.read()
 
     return np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
+
